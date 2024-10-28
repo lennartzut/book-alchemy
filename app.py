9df610126cd1fb8d0276641337315246a9f8175c
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-10-28 13:49:31 +0100

    Add data_models.py and defined models, created tables

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from flaks_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from data_models import db, Author, Book
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///data/library'
-                                         '.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://data/library.sqlite'
 
 db.init_app(app)
 
+# with app.app_context():
+#     db.create_all()
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5002, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5002, debug=True)
---
+++ data_models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120))
+    birth_date = db.Column(db.DateTime)
+    date_of_death = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"Author(id={self.id}, name={self.name})"
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(20))
+    title = db.Column(db.String(200))
+    publication_year = db.Column(db.DateTime)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+
+    def __repr__(self):
+        return f"Book(id={self.id}, title={self.title})"
