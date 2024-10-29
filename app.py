0409818a704e0a8ccba29441ece8820d8bb691e2
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-10-29 14:30:59 +0100

    Add 'add_book' route and updated the html template accordingly

--- app.py
+++ app.py
@@ -24,17 +24,40 @@ def add_author():
         date_of_death = datetime.strptime(date_of_death_str,
                                           '%Y-%m-%d') if date_of_death_str else None
 
-        new_author = Author(name=name, birth_date=birthdate,
+        new_author = Author(name=name,
+                            birth_date=birthdate,
                             date_of_death=date_of_death)
         db.session.add(new_author)
         db.session.commit()
 
-        flash(
-            "Author successfully added!")  # Flash the success message
-
+        flash("Author successfully added!")
         return redirect(url_for('add_author'))
 
     return render_template('add_author.html')
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        isbn = request.form['isbn']
+        publication_year_str = request.form['publication_year']
+        author_id = request.form['author_id']
+        publication_year = datetime.strptime(publication_year_str,
+                                             '%Y')
+
+        new_book = Book(title=title,
+                        isbn=isbn,
+                        publication_year=publication_year,
+                        author_id=author_id)
+        db.session.add(new_book)
+        db.session.commit()
+
+        flash("Book successfully added!")
+        return redirect(url_for('add_book'))
+
+    authors = Author.query.all()
+    return render_template('add_book.html', authors=authors)
 
 
 if __name__ == '__main__':
